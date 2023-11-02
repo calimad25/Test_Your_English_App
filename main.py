af127f1481dd104242da099ebc74c8046041600b
calimad <calimanescuadrian@gmail.com>
2023-11-02 22:15:33 +0200

    Translate the words and create a csv

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+import pandas
+import random
+
+# https://en.wiktionary.org/wiki/Wiktionary:Frequency_lists/Contemporary_fiction
+# The source of words. First 500.
+# Translated the words in GoogleSheets with =GOOGLETRANSLATE(text,"ro","en")
+
+
