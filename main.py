9ff52e344e1db309eb9d96a6071e84fe5cad9f5c
calimad <calimanescuadrian@gmail.com>
2023-11-02 22:16:41 +0200

    Start creating the UI

--- main.py
+++ main.py
@@ -6,4 +6,13 @@ import random
 # The source of words. First 500.
 # Translated the words in GoogleSheets with =GOOGLETRANSLATE(text,"ro","en")
 
+BACKGROUND_COLOR = "#B1DDC6"
 
+# ________ UI _________ #
+# Window
+window = Tk()
+window.title("Test Your English App")
+window.config(pady=50, padx=50, bg=BACKGROUND_COLOR)
+
+
+window.mainloop()
